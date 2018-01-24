be271fdac6c3e37fa32ebb7346d0caa07af9a7c1
macbook <isaevnextdoor@gmail.com>
2018-01-24 14:52:51 +0300

    lab4

---
+++ inc/memlayout.h
@@ -0,0 +1,69 @@
+#ifndef JOS_INC_MEMLAYOUT_H
+#define JOS_INC_MEMLAYOUT_H
+
+#ifndef __ASSEMBLER__
+#include <inc/types.h>
+#include <inc/mmu.h>
+#endif /* not __ASSEMBLER__ */
+
+/*
+ * This file contains definitions for memory management in our OS,
+ * which are relevant to both the kernel and user-mode software.
+ */
+
+// Global descriptor numbers
+#define GD_KT     0x08     // kernel text
+#define GD_KD     0x10     // kernel data
+#define GD_UT     0x18     // user text
+#define GD_UD     0x20     // user data
+#define GD_TSS0   0x28     // Task segment selector for CPU 0
+
+// All physical memory mapped at this address
+#define	KERNTOP  0x0000
+#define	KERNBASE 0x1000
+
+#define KSTKSIZE	(8*PGSIZE)   		// size of a kernel stack
+
+#define ULIM   0x220000
+
+/*
+ * User read-only mappings! Anything below here til UTOP are readonly to user.
+ * They are global pages mapped in at env allocation time.
+ */
+
+// User read-only virtual page table (see 'uvpt' below)
+#define UVPT		(ULIM - PTSIZE)
+// Read-only copies of the Page structures
+#define UPAGES		(UVPT - PTSIZE)
+// Read-only copies of the global env structures
+#define UENVS		(UPAGES - PTSIZE)
+
+/*
+ * Top of user VM. User can manipulate VA from UTOP-1 and down!
+ */
+
+// Top of user-accessible VM
+#define UTOP		UENVS
+// Top of one-page user exception stack
+#define UXSTACKTOP	UTOP
+// Next page left invalid to guard against exception stack overflow; then:
+// Top of normal user stack
+#define USTACKTOP	(UTOP - 2*PGSIZE)
+
+// Where user programs generally begin
+#define UTEXT		(2*PTSIZE)
+
+// Used for temporary page mappings.  Typed 'void*' for convenience
+#define UTEMP		((void*) PTSIZE)
+// Used for temporary page mappings for the user page-fault handler
+// (should not conflict with other temporary page mappings)
+#define PFTEMP		(UTEMP + PTSIZE - PGSIZE)
+// The location of the user-level STABS data structure
+
+#ifndef __ASSEMBLER__
+
+typedef uint32_t pte_t;
+typedef uint32_t pde_t;
+
+#endif /* !__ASSEMBLER__ */
+#endif /* !JOS_INC_MEMLAYOUT_H */
---
+++ kern/kclock.c
@@ -0,0 +1,38 @@
+/* See COPYRIGHT for copyright information. */
+
+#include <inc/x86.h>
+#include <kern/kclock.h>
+
+void
+rtc_init(void)
+{
+	nmi_disable();
+	// LAB 4: your code here
+
+	outb(0x70, RTC_BREG); // set to reg b
+	uint8_t reg_b = inb(0x71); // read reg b
+	outb(0x70, RTC_BREG); // reset to reg b
+	outb(0x71, reg_b | RTC_PIE); // write reg b
+
+	// task 5
+	outb(0x70, RTC_AREG); // set to reg a
+	uint8_t reg_a = inb(0x71); // read reg a
+	outb(0x70, RTC_AREG); // set to reg a
+	outb(0x71, reg_a | 15); // write reg a
+
+	//irq_setmask_8259A(IRQ_CLOCK);
+	nmi_enable();
+}
+
+uint8_t
+rtc_check_status(void)
+{
+	uint8_t status = 0;
+	// LAB 4: your code here
+
+	outb(0x70, RTC_CREG); // set to reg c
+	status = inb(0x71); // read reg c
+
+	return status;
+}
+
